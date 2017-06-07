02fb0f440424a3824061a9c9e3c143aa9cab498f
Wismin Effendi <wismin.effendi@gmail.com>
2017-06-07 08:19:40 -0500

    Create the custom control without interactivity

---
+++ Custom Control - Reusable Slider/RangeSlider.swift
@@ -0,0 +1,70 @@
+//
+//  RangeSlider.swift
+//  Custom Control - Reusable Slider
+//
+//  Created by Wismin Effendi on 6/7/17.
+//  Copyright © 2017 iShinobi. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class RangeSlider: UIControl {
+
+    var minimumValue = 0.0
+    var maximumValue = 1.0
+    var lowerValue = 0.2
+    var upperValue = 0.8
+    
+    let trackLayer = CALayer()
+    let lowerThumbLayer = CALayer()
+    let upperThumbLayer = CALayer()
+    
+    var thumbWidth: CGFloat {
+        return CGFloat(bounds.height)
+    }
+    
+    override var frame: CGRect {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        trackLayer.backgroundColor = UIColor.blue.cgColor
+        layer.addSublayer(trackLayer)
+        
+        lowerThumbLayer.backgroundColor = UIColor.green.cgColor
+        layer.addSublayer(lowerThumbLayer)
+        
+        upperThumbLayer.backgroundColor = UIColor.green.cgColor
+        layer.addSublayer(upperThumbLayer)
+        
+        updateLayerFrames()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func updateLayerFrames() {
+        trackLayer.frame = bounds.insetBy(dx: 0.0, dy: bounds.height / 3)
+        trackLayer.setNeedsDisplay()
+        
+        let lowerThumbCenter = CGFloat(positionForValue(lowerValue))
+        lowerThumbLayer.frame = CGRect(x: lowerThumbCenter - thumbWidth / 2.0, y: 0.0, width: thumbWidth, height: thumbWidth)
+        lowerThumbLayer.setNeedsDisplay()
+        
+        let upperThumbCenter = CGFloat(positionForValue(upperValue))
+        upperThumbLayer.frame = CGRect(x: upperThumbCenter - thumbWidth / 2.0, y: 0.0, width: thumbWidth, height: thumbWidth)
+        upperThumbLayer.setNeedsDisplay()
+        
+        
+    }
+    
+    func positionForValue(_ value: Double) -> Double {
+        return Double(bounds.width - thumbWidth) * (value - minimumValue) / (maximumValue - minimumValue) + Double(thumbWidth / 2.0)
+    }
+ }
--- Custom Control - Reusable Slider/ViewController.swift
+++ Custom Control - Reusable Slider/ViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let rangeSlider = RangeSlider(frame: CGRect.zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        rangeSlider.backgroundColor = .red
+        view.addSubview(rangeSlider)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        let margin: CGFloat = 20.0
+        let width = view.bounds.width - 2.0 * margin
+        rangeSlider.frame = CGRect(x: margin, y: margin + topLayoutGuide.length, width: width, height: 31.0)
     }
 
 
